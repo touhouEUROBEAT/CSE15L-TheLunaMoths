7410084018eb740d085496df7f5fb9630f767023
kjchou324 <kjchou@ucsd.edu>
2022-01-26 18:34:16 -0800

    created test for test file 1

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,24 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testFile() throws IOException {
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        ArrayList<String> expectedLinks = new ArrayList<String>();
+        expectedLinks.add("https://something.com");
+        expectedLinks.add("some-page.html");
+        assertEquals(expectedLinks, links);
     }
 }
